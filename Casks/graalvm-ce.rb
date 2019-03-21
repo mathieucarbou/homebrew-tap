ac96a4db9f18b25f751346720bd58d0a59983de8
Mathieu Carbou <mathieu.carbou@gmail.com>
2019-03-21 18:36:35 -0400

    Update graalvm-ce.rb

--- Casks/graalvm-ce.rb
+++ Casks/graalvm-ce.rb
@@ -2,7 +2,7 @@ cask 'graalvm-ce' do
   version '1,0.0:14'
   sha256 'e808adc49cba9e1f378d887c7723b1134e2c67520ae69ff6ffd9c261d536c036'
 
-  # github.com/AdoptOpenJDK was verified as official when first introduced to the cask
+  # github.com/mathieucarbou was verified as official when first introduced to the cask
   url "https://github.com/oracle/graal/releases/download/vm-#{version.before_comma}.#{version.after_comma.before_colon}-rc#{version.after_colon}/graalvm-ce-#{version.before_comma}.#{version.after_comma.before_colon}-rc#{version.after_colon}-macos-amd64.tar.gz"
   appcast 'https://github.com/oracle/graal/releases.atom'
   name 'GraalVM Community Edition'
