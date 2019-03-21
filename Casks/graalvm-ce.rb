2124e802b45eb408cb2fa6db889d7d5321a2631b
Mathieu Carbou <mathieu.carbou@gmail.com>
2019-03-21 17:24:39 -0400

    Update graalvm-ce.rb

--- Casks/graalvm-ce.rb
+++ Casks/graalvm-ce.rb
@@ -1,6 +1,6 @@
 cask 'graalvm-ce' do
   version '1,0.0:14'
-  sha256 'ba7dc2ff4e43185938ea3c5debf6f4bdc780c75b1e86209284b902a20eca9e684c361085f1c8162b9240dee548838404'
+  sha256 'e808adc49cba9e1f378d887c7723b1134e2c67520ae69ff6ffd9c261d536c036'
   url "https://github.com/oracle/graal/releases/download/vm-#{version.before_comma}.#{version.after_comma.before_colon}-rc#{version.after_colon}/graalvm-ce-#{version.before_comma}.#{version.after_comma.before_colon}-rc#{version.after_colon}-macos-amd64.tar.gz"
   appcast 'https://github.com/oracle/graal/releases.atom'
   name 'GraalVM Community Edition'
