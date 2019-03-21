366f159f88d6707456d1a0f9609881e298026eee
Mathieu Carbou <mathieu.carbou@gmail.com>
2019-03-21 17:44:05 -0400

    Update graalvm-ce.rb

--- Casks/graalvm-ce.rb
+++ Casks/graalvm-ce.rb
@@ -1,10 +1,13 @@
 cask 'graalvm-ce' do
   version '1,0.0:14'
   sha256 'e808adc49cba9e1f378d887c7723b1134e2c67520ae69ff6ffd9c261d536c036'
+
+  # github.com/AdoptOpenJDK was verified as official when first introduced to the cask
   url "https://github.com/oracle/graal/releases/download/vm-#{version.before_comma}.#{version.after_comma.before_colon}-rc#{version.after_colon}/graalvm-ce-#{version.before_comma}.#{version.after_comma.before_colon}-rc#{version.after_colon}-macos-amd64.tar.gz"
   appcast 'https://github.com/oracle/graal/releases.atom'
   name 'GraalVM Community Edition'
   homepage 'https://www.graalvm.org/'
+
   postflight do
     system_command '/bin/mv',
                    args: [
@@ -14,5 +17,6 @@ cask 'graalvm-ce' do
                          ],
                    sudo: true
   end
+
   uninstall delete: "/Library/Java/JavaVirtualMachines/graalvm-ce-#{version.before_comma}.#{version.after_comma.before_colon}-rc#{version.after_colon}"
 end
